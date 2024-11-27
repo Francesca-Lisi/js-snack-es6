c08c79e3cfdc4e442cb8df93d6d74bd68216da0d
Francesca-Lisi <francescalisi2143@gmail.com>
2024-11-27 18:34:59 +0100

    feat: add array of objects in Snack1

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -2,3 +2,34 @@
 // Ogni oggetto descriverà una bici da corsa 
 // con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+
+const biciclette = [
+  {
+    nome: 'Tarmac',
+    peso: 7.9
+  },
+  {
+    nome: 'Roubaix',
+    peso: 8.2
+  },
+  {
+    nome: 'Dogma',
+    peso: 10.1
+  },
+  {
+    nome: 'Aethos',
+    peso: 6.9
+  },
+  {
+    nome: 'Madone',
+    peso: 7.6
+  },
+  {
+    nome: 'Reacto',
+    peso: 9.8
+  },
+  {
+    nome: 'F7',
+    peso: 8.8
+  }
+]
